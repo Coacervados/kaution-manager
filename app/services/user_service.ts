828e31301910d546d5467eed3d178fa4870c1b04
Isaac Sales <isaacrodrigues1516@gmail.com>
2025-04-30 23:55:25 -0300

    user service started

--- app/services/user_service.ts
+++ app/services/user_service.ts
@@ -1,4 +1,33 @@
+import User from "#models/user";
 
-export class UserService {
-  
+interface UserInput {
+    name: string;
+    email: string;
+    password: string;
+}
+
+export default class UserService {
+  static async create(data: UserInput) {
+    return User.create(data);
+  }
+
+  static async findAll() {
+    return User.all();
+  }
+
+  static async findById(id: number) {
+    return User.find(id);
+  }
+
+  static async update(id: number, data: UserInput) {
+    const user = await User.find(id);
+
+    return user?.merge(data).save();
+  }
+
+  static async delete(id: number) {
+    const user = await User.find(id);
+
+    return user?.delete();
+  }
 }
