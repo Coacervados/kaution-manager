1c1a413d49d1e6969fa3ffa98ff525a503d6e655
Isaac Sales <124395470+IsaacSales15@users.noreply.github.com>
2025-05-05 08:46:17 -0300

    controllers

--- app/controllers/users_controller.ts
+++ app/controllers/users_controller.ts
@@ -3,14 +3,23 @@ import UserService from '#services/user_service'
 import { registerUserValidator } from '#validators/auth'
 
 export default class UsersController {
+  async create({ request, response }: HttpContext) {
+    const data = await request.validateUsing(registerUserValidator)
+    const user = await UserService.create(data)
+    console.log(user)
+    return response.created(user)
+  }
 
-    public async create({ request, response }: HttpContext) {
-        const data = await request.validateUsing(registerUserValidator)
-        const user = await UserService.create(data)
-        return response.created(user);
-    }
+  async getAll({ response }: HttpContext) {
+    return response.ok(UserService.findAll())
+  }
 
-    public async get({ response }: HttpContext) {
-        return response.ok(UserService.findAll());
-    }
+  async getById({ params, response }: HttpContext) {
+    return response.ok(UserService.findById(params.id))
+  }
+
+  async update({ params, request, response }: HttpContext) {
+    const data = await request.validateUsing(registerUserValidator)
+    return response.ok(UserService.update(params.id, data))
+  }
 }
--- start/routes.ts
+++ start/routes.ts
@@ -1,7 +1,7 @@
 import router from '@adonisjs/core/services/router'
 const UserController = () => import('#controllers/users_controller')
 
-router.post('users', [UserController, 'create'])
-router.get('users', [UserController, 'get'])
+router.post('/users', [UserController, 'create'])
+router.get('/users', [UserController, 'getAll'])
 
 router.on('/').renderInertia('home')
