54fb730df9b8be71d0259897a7af4eb3fd35fa14
Isaac Sales <124395470+IsaacSales15@users.noreply.github.com>
2025-05-14 13:51:28 -0300

    Category service

--- app/controllers/categories_controller.ts
+++ app/controllers/categories_controller.ts
@@ -1,5 +1,71 @@
-// import type { HttpContext } from '@adonisjs/core/http'
+import Category from '#models/category'
+import {
+  ConflictError,
+  DatabaseError,
+  NotFoundErr,
+} from '#exceptions/api_error_exception'
+
+interface CategoryInput {
+  name: string
+  description: string
+  inventoryId: number
+}
 
 export default class CategoriesController {
-  
+  async create(CategoryInput: CategoryInput) {
+    try {
+      return await Category.create(CategoryInput)
+    } catch (error) {
+      if (error.code === 'ER_DUP_ENTRY') {
+        throw new ConflictError('Email already exists')
+      }
+      throw new DatabaseError('Error when creating category')
+    }
+  }
+
+  async index(): Promise<Category[]> {
+    try {
+      return await Category.all()
+    } catch (error) {
+      if (error instanceof DatabaseError) throw error
+      throw new DatabaseError('Error when fetching categories')
+    }
+  }
+
+  async show(id: number): Promise<Category | null> {
+    try {
+      return await Category.find(id)
+    } catch (error) {
+      if (error instanceof NotFoundErr) throw error
+      throw new DatabaseError('Error when fetching category')
+    }
+  }
+
+  async update(id: number, data: CategoryInput): Promise<Category> {
+    try {
+      const category = await Category.find(id)
+      if (!category) {
+        throw new NotFoundErr('Category not found')
+      }
+      category.merge(data)
+      await category.save()
+      return category
+    } catch (error) {
+      if (error instanceof NotFoundErr) throw error
+      throw new DatabaseError('Error when updating category')
+    }
+  }
+
+  async destroy(id: number): Promise<void> {
+    try {
+      const category = await Category.find(id)
+      if (!category) {
+        throw new NotFoundErr('Category not found')
+      }
+      await category.delete()
+    } catch (error) {
+      if (error instanceof NotFoundErr) throw error
+      throw new DatabaseError('Error when deleting category')
+    }
+  }
 }
